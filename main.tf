59c456403fcf62401e0638a81a0679f9a0621ff0
Faiz Sultan <85805423+FaizReza@users.noreply.github.com>
2026-02-07 21:11:37 +0530

    Clean up whitespace in main.tf
    Removed unnecessary blank lines in main.tf.

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-
 }
 
 data "aws_ami" "myami"{
@@ -13,18 +12,14 @@ filter {
     values = ["amzn2-ami-hvm*"]
   }
 
-
 }
 
 resource "aws_instance" "myec2" {
   ami    = data.aws_ami.myami.id 
   instance_type = "t2.nano"
-
 }
 
 resource "aws_instance" "myec2-2" {
   ami    = data.aws_ami.myami.id 
   instance_type = "t2.nano"
-
- 
 }
